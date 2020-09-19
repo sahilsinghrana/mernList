82ae10f3891d7a89468f5f17d3b08b899774fc55
sahilsinghrana <ranasahil98@gmail.com>
2020-09-19 23:35:13 +0530

    1

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -9,30 +9,39 @@ import {
 export const getItems = () => (dispatch) => {
   dispatch(setItemsLoading());
 
-  axios.get("/api/items").then((res) =>
-    dispatch({
-      type: GET_ITEMS,
-      payload: res.data,
-    })
-  );
+  axios
+    .get("/api/items")
+    .then((res) =>
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data,
+      })
+    )
+    .catch((e) => console.log(e));
 };
 
 export const deleteItem = (id) => (dispatch) => {
-  axios.delete(`/api/items/${id}`).then((res) =>
-    dispatch({
-      type: DELETE_ITEM,
-      payload: id,
-    })
-  );
+  axios
+    .delete(`/api/items/${id}`)
+    .then((res) =>
+      dispatch({
+        type: DELETE_ITEM,
+        payload: id,
+      })
+    )
+    .catch((e) => console.log(e));
 };
 
 export const addItem = (item) => (dispatch) => {
-  axios.post("/api/items", item).then((res) =>
-    dispatch({
-      type: ADD_ITEM,
-      payload: res.data,
-    })
-  );
+  axios
+    .post("/api/items", item)
+    .then((res) =>
+      dispatch({
+        type: ADD_ITEM,
+        payload: res.data,
+      })
+    )
+    .catch((e) => console.log(e));
 };
 
 export const setItemsLoading = () => {
